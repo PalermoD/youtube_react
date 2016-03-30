6572bdfa43de43540143645295ad071dfb496684
PalermoD <palermo.deschamps@gmail.com>
2016-03-30 11:46:14 -0400

    server

--- server.js
+++ server.js
@@ -23,8 +23,8 @@ if (isDevelopment) {
   new WebpackDevServer(webpack(config), {
     publicPath: config.output.publicPath,
     hot: true
-  }).listen(3000, 'localhost', function (err, result) {
+  }).listen(8080, 'localhost', function (err, result) {
     if (err) { console.log(err) }
-    console.log('Listening at localhost:3000');
+    console.log('Listening at localhost:8080');
   });
 }
